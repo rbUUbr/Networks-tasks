d9a6950603bb96ea54de386de6a2f4d6983f9d4e
rbUUbr <kirabsuir@gmail.com>
2017-03-20 00:36:19 +0300

    Temp removed parsing of URL and added OptParser

--- my_curl.rb
+++ my_curl.rb
@@ -1,14 +1,12 @@
+#!/usr/bin/env ruby
 require 'net/http'
+require 'optparse'
 require 'uri'
 require_relative 'parse_methods.rb'
 require 'colorize'
 include Parser
-uri = URI.parse("#{ARGV[0]}")
-response = Net::HTTP.get_response(uri)
-is_correct?(response)
-if !(response.code =~ /2[0-9][0-9]/)
-  puts "Error!, code of error is: #{response.code}\n".color(:red)
-else
-  puts "All is correct, code is #{response.code}\n".colorize(:green)
-  puts "#{response.body}".colorize(:blue)
-end
+options = {}
+#uri = URI.parse("#{ARGV[0]}")
+#response = Net::HTTP.get_response(uri)
+#is_correct?(response)
+switch_for_first_param
--- parse_methods.rb
+++ parse_methods.rb
@@ -1,12 +1,19 @@
 module Parser
 	def is_correct?(response)
 		if !(response.code =~ /2[0-9][0-9]/)
-			puts "Error!, code of error is: #{response.code}\n".color(:red)
-			return false
+			STDOUT.puts "Error!, code of error is: #{response.code}\n".color(:red)
 		else
-			puts "All is correct, code is #{response.code}\n".colorize(:green)
-			puts "#{response.body}".colorize(:blue)
-			return true
+			STDOUT.puts "All is correct, code is #{response.code}\n".colorize(:green)
+			STDOUT.puts "#{response.body}".colorize(:blue)
 		end
 	end
+	def switch_for_first_param
+		options = {}
+		OptionParser.new do |parser|
+			parser.banner = "Usage: my_curl.rb [options] URL"
+			parser.on("-h", "--help", "Shows this help message") do ||
+				puts parser
+			end
+		end.parse!
+  end
 end
